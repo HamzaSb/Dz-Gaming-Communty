f1d8d21629390ae88a14a099570c977c87fa1d00
HamzaSb <45317520+HamzaSb@users.noreply.github.com>
2018-11-25 21:37:46 +0100

    Update bc.js

--- bc.js
+++ bc.js
@@ -18,9 +18,9 @@ client.on("message", message => {
 
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
+      console.log(`Desert Bot- Script By : Niga`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+                  console.log(`ON ${client.guilds.size} Servers '     Script By : Niga ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`اسم سيرفرك`,"http://twitch.tv/S-F")
